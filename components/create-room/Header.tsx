a58c08d1ed4bd40f0205e5c1a93316548a1800a0
황혜경 <hwangmari@naver.com>
2025-12-05 10:23:17 +0900

    [황총무] 스타일 수정

--- components/create-room/Header.tsx
+++ components/create-room/Header.tsx
@@ -7,6 +7,11 @@ export default function Header() {
       </h1>
       <p className="text-sm text-gray-400">
         약속 잡기 힘드시죠? 황총무가 깔끔하게 정리해드려요!
+        <br />
+        소거법으로 <strong className="text-red-400">
+          안 되는 날
+        </strong> 빼고 <strong className="text-blue-400">되는 날</strong>을
+        정해보세욥 'ㅅ'/
       </p>
     </div>
   );
